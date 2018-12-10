b4d8b4e6ca5e66026cae44394f5b321e209dffa7
Lingyun Zheng <christinezly@gmail.com>
2018-12-09 23:11:44 -0500

    Initial Commit

---
+++ choice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  choice
+//
+//  Created by Ling on 12/9/18.
+//  Copyright © 2018 Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
