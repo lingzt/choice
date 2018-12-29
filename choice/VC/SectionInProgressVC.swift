dae4f627a0f06431ed64c234ca3321033ed72265
Ling <Ling@Lings-MBP.attlocal.net>
2018-12-29 14:51:44 -0500

    adjust setup VC by adding stackview in the bottom

---
+++ choice/VC/SectionInProgressVC.swift
@@ -0,0 +1,30 @@
+//
+//  SectionInProgressViewController.swift
+//  choice
+//
+//  Created by Ling on 12/29/18.
+//  Copyright © 2018 Ling. All rights reserved.
+//
+
+import UIKit
+
+class SectionInProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
