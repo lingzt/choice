ad08687fb6fa5fdccb00d048f1b1b3eddf353cae
Ling <Ling@Lings-MacBook-Pro.local>
2019-01-19 18:43:42 -0500

    LT adjust collection view

--- choice/VC/SectionSetupVC.swift
+++ choice/VC/SectionSetupVC.swift
@@ -14,7 +14,7 @@ class SectionSetupVC: UIViewController {
     
     var candidateList = [Candidate]()
     var candidateListWithPlaceHolder = [Candidate]()
-    var candiatePlaceHolder = Candidate(candidateID: 0, title: "somename", image: UIImage(named: "cross"))
+    var candiatePlaceHolder = Candidate(candidateID: 0, title: "somename", image: UIImage(named: "addPhotoIcon"))
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
@@ -23,22 +23,22 @@ class SectionSetupVC: UIViewController {
         collectionView.register(nib, forCellWithReuseIdentifier: "CandidateCollectionViewCell")
         candidateListWithPlaceHolder = [candiatePlaceHolder] + candidateList
     }
-    
 }
 
 
 extension SectionSetupVC: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return candidateListWithPlaceHolder.count
+        return 10
+//        return candidateListWithPlaceHolder.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: "CandidateCollectionViewCell"), for: indexPath) as? CandidateCollectionViewCell else {return UICollectionViewCell()}
-        cell.setupCell(candidate: candidateListWithPlaceHolder[indexPath.row])
+        cell.setupCell(candidate: candidateListWithPlaceHolder[0])
 //        cell.Delegte = self
         
         return cell
@@ -51,4 +51,10 @@ extension SectionSetupVC: UICollectionViewDelegate {
         
     }
 
+}
+
+extension SectionSetupVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.collectionView.frame.width/3.5, height: self.collectionView.frame.height/2.9)
+    }
 }
